45fa83878191a78d983d252ab617124d6dffb4ff
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-01 17:03:22 +0530

    [Omkar] Add : Employee wage problem using the switch case

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -69,14 +69,52 @@ def emp_dly_wage():
         full_time_wage = wage_per_hr * full_time_wk_hr
         print(f"Employee Daily Full Time Wage is : {full_time_wage}")
 
+
+def UsingSwitchCase():  #UC 4
+    """
+        Description:
+            This function use to Calculate the Daily full time or part-time wage
+
+        Parameter:
+            None
+
+        Return: None
+    """
+
+    atd = random_check()
+    match atd:
+        case 0:
+            part_time_wk_wage = wage_per_hr * part_time_wk_hr
+            print(f"Employee Doing Part Time work it's wage is {part_time_wk_wage}")
+        case 1:
+            full_time_wk_wage = wage_per_hr * full_time_wk_hr
+            print(f"Employee Doing Full time work it's wage is : {full_time_wk_wage}")
+        case 2:
+            print("Employee Absent ")
+
+
 def main():
+    """
+           Description:
+               This function use to call the other function and perform the task
+
+           Parameter:
+               None
+
+           Return: None
+       """
+
     print("Welcome To Employee Wage Computation Program ")
 
-    if check_attendence():
-        emp_dly_wage()
-    else:
-        print("Employee Daily Wage is 0 ")
+    # if check_attendence():
+    #     emp_dly_wage()
+    # else:
+    #     print("Employee Daily Wage is 0 ")
 
+    # UsingSwitchCase()
+
+    # Cal_wage_for_month()
+    total_wk_hr_or_days_reached_for_month()
 
 if __name__=="__main__":
     main()
