c99a3a967d4f770f262bb1694cdc9ac037706925
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-01 14:20:27 +0530

    [Omkar] Add : python program to check Employee and print the daily wage salary

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -22,15 +22,36 @@ def check_attendence():
         Parameter:
             None
 
-        Return: None
+        Return: Boolean Value
     """
 
     ran_int = random.randint(0,1)
     if ran_int == 1:
         print("Employee Present ")
+        return True
     else:
         print("Employee Not Present ")
 
+def emp_dly_wage():
+    """
+        Description:
+            This function use to Calculate the Daily Employee Wage
+
+        Parameter:
+            None
+
+        Return: None
+    """
+    wage_per_hr = 20
+    working_hr = 8
+    wage = wage_per_hr * working_hr
+    print("Employee Daily wage is : ", wage)
+
+
 if __name__=="__main__":
     print("Welcome To Employee Wage Computation Program ")
-    check_attendence()
+
+    if check_attendence():
+        emp_dly_wage()
+    else:
+        print("Employee Daily Wage is 0 ")
